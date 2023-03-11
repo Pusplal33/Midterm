2dc6da85782948a50d58e5cb7a17fe821262a96a
Pusplal33 <spusplal2020@gmail.com>
2023-03-10 19:28:39 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Pusplal:uCtsLuxaRdadM2v6@cluster0.vxudc2k.mongodb.net/?retryWrites=true&w=majority"
+};
+
+// uCtsLuxaRdadM2v6 password
